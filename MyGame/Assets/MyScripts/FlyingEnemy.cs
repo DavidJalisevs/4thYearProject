756640c9e33a777f0e1c17bfba776c9731b1af1d
David Jalisevs <58332574+DavidJalisevs@users.noreply.github.com>
2023-04-24 23:42:01 +0100

    build uploaded

--- MyGame/Assets/MyScripts/FlyingEnemy.cs
+++ MyGame/Assets/MyScripts/FlyingEnemy.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class FlyingEnemy : MonoBehaviour
 {
